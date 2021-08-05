1612a5c92c4b28c0ef69eccf0619380d803655f3
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-05 11:48:57 -0300

    add img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,12 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <Head>
-      <title>move.it | Início</title>
-    </Head>
+    <>
+      <Head>
+        <title>move.it | Início</title>
+      </Head>
+
+      <h1>hello World</h1>
+    </>
   )
 }
