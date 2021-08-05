dea61f1d3d209915a90f46bf059855e9bac80dc9
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-05 11:39:05 -0300

    css global e _App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Component {...pageProps} />
+  )
 }
-export default MyApp
+
