f2e77205007d796915c0a641aa92292b7e635256
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-06 12:27:35 -0300

    css do experience bar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -7,6 +7,7 @@ export function ExperienceBar() {
             <div>
                 <div />
             </div>
+            <span>xp</span>
         </header>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { ExperienceBar } from '../components/ExperienceBar'
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       </Head>
 
       <h1>hello World</h1>
+      <ExperienceBar />
     </>
   )
 }
