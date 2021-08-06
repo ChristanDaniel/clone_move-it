ed73b9fbc5b7a05dfb8648777b67163c1746f5f8
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-06 10:29:30 -0300

    component ExperienceBar

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/ExperienceBar.module.css'
+
+export function ExperienceBar() {
+    return(
+        <header className={styles.experienceBar}>
+            <span>0 xp</span>
+            <div>
+                <div />
+            </div>
+        </header>
+    )
+}
