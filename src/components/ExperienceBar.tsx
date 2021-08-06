c961f77dcb0a65ce620c59bc1ea4da261adb716e
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-06 13:05:00 -0300

    experienceBar finalizado

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,11 +1,21 @@
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
+    // const { currentExperience, experienceToNextLevel } = useChallenges();
+
+    // const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
     return(
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div />
+                {/* <div style={{ width: `${percentToNextLevel}%`}}/> */}
+
+                {/* { currentExperience > 0 && (
+                    <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%`}}>
+                        {currentExperience} xp
+                    </span>
+                )} */}
             </div>
             <span>xp</span>
         </header>
