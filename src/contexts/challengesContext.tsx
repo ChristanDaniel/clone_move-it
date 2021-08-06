0c055452b688bbc0e789c60c8e281a7db993711a
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-06 13:31:06 -0300

    challenges criado

--- src/contexts/challengesContext.tsx
+++ src/contexts/challengesContext.tsx
@@ -1,5 +1,12 @@
 import { useState, ReactNode, useEffect } from 'react';
+import challenges from '../../challenges.json';
 import Cookies from 'js-cookie'
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 
 interface ChallengesProviderProps {
     children: ReactNode;
@@ -13,9 +20,18 @@ export function ChallengesProvider( {...rest}: ChallengesProviderProps ) {
     const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
     const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0 )
 
+    const [activeChallenge, setActiveChallenge] = useState<Challenge>() //null <-- não esquece
+
     useEffect(() => {
         Cookies.set('level', String(level))
         Cookies.set('currentExperience', String(challengesCompleted))
         Cookies.set('challengesCompleted', String(challengesCompleted))
     }, [level, currentExperience, challengesCompleted])
+
+    function startNewChallenge() {
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge as Challenge);
+    }
 }
