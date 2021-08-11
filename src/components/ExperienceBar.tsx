6a7b72e4db90b7d7ce5bd532b5c75cc4a7939bb4
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-11 12:16:36 -0300

    Component Profile

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,21 +1,22 @@
+import { useChallenges } from '../hooks/useChallenges';
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
-    // const { currentExperience, experienceToNextLevel } = useChallenges();
+    const { currentExperience, experienceToNextLevel } = useChallenges();
 
-    // const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+    const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
 
     return(
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                {/* <div style={{ width: `${percentToNextLevel}%`}}/> */}
+                <div style={{ width: `${percentToNextLevel}%`}}/>
 
-                {/* { currentExperience > 0 && (
+                { currentExperience > 0 && (
                     <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%`}}>
                         {currentExperience} xp
                     </span>
-                )} */}
+                )}
             </div>
             <span>xp</span>
         </header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
 
       <h1>hello World</h1>
       <ExperienceBar />
+      <Profile />
     </>
   )
 }
