0a358fef0314f11de4499fcada87dc44300b664a
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-12 09:48:56 -0300

    hooks useEffect criados

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -1,13 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useChallenges } from "../hooks/useChallenges";
 
 
-export function CountDownContext() {
+let countdownTimeout: NodeJS.Timeout
+
+export function CountdownContext() {
     const { startNewChallenge } = useChallenges();
 
     const [ time, setTime ] = useState(0.1 * 60);
     const [ isActive, setIsActive ] = useState(false);
     const [hasFinished, setHasFinished ] = useState(false);
+
+    useEffect(() => {
+        window.onbeforeunload = () => {
+            if (isActive) {
+                return 'Você perderá o progresso do countdown até aqui, tem certeza?'
+            }
+        }
+    }, [isActive])
+
+    useEffect(() => {
+        if (isActive && time > 0) {
+            countdownTimeout = setTimeout(() => {
+                setTime(time - 1);
+            }, 1000);
+        } else if (isActive && time === 0) {
+            startNewChallenge();
+            setHasFinished(true);
+            setIsActive(false);
+        }
+    }, [isActive, time])
 
 
 
