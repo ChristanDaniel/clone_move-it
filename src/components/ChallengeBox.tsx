836975a14e38a4e8a7b3c2461a936136f2069adb
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-13 13:57:48 -0300

    component ChallengeBox finalizado

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -21,6 +21,46 @@ export function ChallengesBox() {
 
     return(
         <div className={styles.challengeContainer}>
+            {activeChallenge? (
+                <div className={styles.chellengeActive}>
+                    <header>Ganhe {activeChallenge.amount} xp</header>
+
+                    <main>
+                        {activeChallenge.type === 'body'
+                        // ? <img src="/icons/body.svg" />
+                        // : <img src="icons/eye.svg" />
+                        }
+                        <strong>Novo desafio</strong>
+                        <p>{activeChallenge.description}</p>
+                    </main>
+
+                    <footer>
+                        <button
+                        type="button"
+                        className={styles.challengeFailedButton}
+                        onClick={handleChallengeFailed}
+                        >
+                            Falhei
+                        </button>
+                        <button
+                        type="button"
+                        className={styles.challengeSucceededButton}
+                        onClick={handleChallengeSucceeded}
+                        >
+                            completei
+                        </button>
+                    </footer>
+                </div>
+
+            ) : (
+                <div className={styles.challengeNotActive}>
+                    <strong>Finalize um ciclo para receber um desafio</strong>
+                    <p>
+                        {/* <img src="icons/level-up.svg" alt="Level up"/> */}
+                        Avance de level completando os desafios.
+                    </p>
+                </div>
+            )}
 
         </div>
     );
