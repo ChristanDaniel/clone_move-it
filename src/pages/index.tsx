360b657ec5d7510cef7e8d13fecf70f03a06ca13
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-15 10:28:37 -0300

    front-end finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import Head from 'next/head'
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
+import { ChallengesBox } from '../components/ChallengeBox';
+
+import styles from '../styles/pages/Home.module.css'
 
 import { ChallengesProvider } from '../contexts/challengesContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 interface HomeProps {
   level: number;
@@ -10,24 +16,32 @@ interface HomeProps {
   challengesCompleted: number;
 }
 
-export default function Home() {
+export default function Home({level, currentExperience, challengesCompleted}: HomeProps ) {
   return (
-    <>
-      <ChallengesProvider
-      level={level}
-      currentExperience={currentExperience}
-      challengesCompleted={challengesCompleted}
-      ></ChallengesProvider>
-      <main>
-        <Head>
-          <title>move.it | Início</title>
-        </Head>
-        
-        <ExperienceBar />
-        
-        <Profile />
-      </main>
+    <ChallengesProvider
+    level={level}
+    currentExperience={currentExperience}
+    challengesCompleted={challengesCompleted}
+    >
+      <CountdownProvider>
+        <main className={styles.container}>
+          <Head>
+            <title>move.it | Início</title>
+          </Head>
+          
+          <ExperienceBar />
+          <section>
+            <div className={styles.cycleContainer}>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
 
-    </>
+            <ChallengesBox />
+          </section>
+        </main>
+      </CountdownProvider>
+    </ChallengesProvider>
+
   )
 }
